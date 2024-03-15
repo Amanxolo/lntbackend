4d5c2fbaf37d56fc45f019715d607fce36bf3bcf
pratik <pratik.phirmuhani@gmail.com>
2024-03-15 18:13:05 +0530

    Code update proj1 almost done

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const port = process.env.PORT || 3000
+
+const proj1router = require('./routers/proj1')
+const proj2router = require('./routers/proj2')
+
+app.use(proj1router)
+app.use(proj2router)
+
+app.post
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
---
+++ src/routers/proj2.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = new express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
